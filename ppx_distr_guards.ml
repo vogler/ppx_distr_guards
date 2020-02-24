0a86873baf42869328926ec2c2073b01d98617f1
Ralf Vogler <ralf.vogler@gmail.com>
2020-02-24 17:11:23 +0100

    migrate to ocaml-migrate-parsetree

--- ppx_distr_guards.ml
+++ ppx_distr_guards.ml
@@ -1,9 +1,12 @@
-open Ast_mapper
-open Ast_helper
-open Asttypes
-open Parsetree
+open Migrate_parsetree
 
-let rec mapper =
+open Ast_409
+let ocaml_version = Versions.ocaml_409
+
+let mapper _config _cookies =
+  let open Ast_mapper in
+  let open Ast_helper in
+  let open Parsetree in
   let case_to_cases m = function
     | { pc_lhs;
         pc_guard = Some guard;
@@ -16,9 +19,9 @@ let rec mapper =
           | _ -> p :: a
         in
         let ps = f [] pc_lhs in
-        let e = mapper.expr mapper pc_rhs in
+        let e = m.expr m pc_rhs in
         List.map (fun p -> Exp.case p ~guard e) ps
-    | x -> [default_mapper.case mapper x]
+    | x -> [default_mapper.case m x]
   in
   let distr_mapper = { default_mapper with
     cases = fun m xs -> List.map (case_to_cases m) xs |> List.flatten }
@@ -26,12 +29,14 @@ let rec mapper =
   { default_mapper with
     expr = fun m expr ->
       match expr with
-      | { pexp_desc = Pexp_extension ({ txt = "distr" }, PStr [{ pstr_desc = Pstr_eval (e,a) }]) } ->
+      | { pexp_desc = Pexp_extension ({ txt = "distr"; _ }, PStr [{ pstr_desc = Pstr_eval (e,a); _ }]); _ } ->
           m.expr distr_mapper { e with pexp_attributes = expr.pexp_attributes @ a }
       | x -> default_mapper.expr m x;
   }
 
-let () = register "distr_guards" (fun _ -> mapper)
+let () =
+  Driver.register ~name:"distr_guards" ocaml_version mapper
+
 (*
 ocamlc -dparsetree test.ml
 ocamlbuild -package compiler-libs.common ppx_distr_guards.native
