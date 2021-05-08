b172224791f6488bb86571ea76f843f60ebf10da
tatchi <corentin.leruth@gmail.com>
2021-05-08 07:55:54 +0200

    migrate to ppxlib

--- ppx_distr_guards.ml
+++ ppx_distr_guards.ml
@@ -1,44 +1,39 @@
-open Migrate_parsetree
+open Ppxlib
 
-open Ast_409
-let ocaml_version = Versions.ocaml_409
+let rec case_to_cases ~ctxt case =
+  match case with
+  | { pc_lhs; pc_guard = Some guard; pc_rhs } ->
+      (* collect or-patterns into list *)
+      (* TODO fold over all constructors to support or-patterns in other patterns *)
+      let rec f a p =
+        match p.ppat_desc with Ppat_or (p1, p2) -> f (f a p2) p1 | _ -> p :: a
+      in
+      let ps = f [] pc_lhs in
+      List.map (fun p -> Ast_helper.Exp.case p ~guard pc_rhs) ps
+  | x -> [ { x with pc_rhs = expand ~ctxt x.pc_rhs } ]
 
-let mapper _config _cookies =
-  let open Ast_mapper in
-  let open Ast_helper in
-  let open Parsetree in
-  let case_to_cases m = function
-    | { pc_lhs;
-        pc_guard = Some guard;
-        pc_rhs
-      } ->
-        (* collect or-patterns into list *)
-        (* TODO fold over all constructors to support or-patterns in other patterns *)
-        let rec f a p = match p.ppat_desc with
-          | Ppat_or (p1, p2) -> f (f a p2) p1
-          | _ -> p :: a
-        in
-        let ps = f [] pc_lhs in
-        let e = m.expr m pc_rhs in
-        List.map (fun p -> Exp.case p ~guard e) ps
-    | x -> [default_mapper.case m x]
-  in
-  let distr_mapper = { default_mapper with
-    cases = fun m xs -> List.map (case_to_cases m) xs |> List.flatten }
-  in
-  { default_mapper with
-    expr = fun m expr ->
-      match expr with
-      | { pexp_desc = Pexp_extension ({ txt = "distr"; _ }, PStr [{ pstr_desc = Pstr_eval (e,a); _ }]); _ } ->
-          m.expr distr_mapper { e with pexp_attributes = expr.pexp_attributes @ a }
-      | x -> default_mapper.expr m x;
-  }
+and function_expand ~ctxt cases =
+  let new_cases = cases |> List.map (case_to_cases ~ctxt) |> List.flatten in
 
-let () =
-  Driver.register ~name:"distr_guards" ocaml_version mapper
+  let loc = Expansion_context.Extension.extension_point_loc ctxt in
+  Ast_helper.Exp.function_ ~loc new_cases
 
-(*
-ocamlc -dparsetree test.ml
-ocamlbuild -package compiler-libs.common ppx_distr_guards.native
-ocamlc -dsource -ppx ./ppx_distr_guards.native test.ml
-*)
+and match_expand ~ctxt expr cases =
+  let new_cases = cases |> List.map (case_to_cases ~ctxt) |> List.flatten in
+  let loc = Expansion_context.Extension.extension_point_loc ctxt in
+  Ast_helper.Exp.match_ ~loc expr new_cases
+
+and expand ~ctxt e =
+  match e.pexp_desc with
+  | Pexp_function cases -> function_expand ~ctxt cases
+  | Pexp_match (e, cases) -> match_expand ~ctxt e cases
+  | _ -> e
+
+let extension =
+  Extension.V3.declare "distr" Extension.Context.expression
+    Ast_pattern.(single_expr_payload __)
+    expand
+
+let rule = Ppxlib.Context_free.Rule.extension extension
+
+let () = Driver.register_transformation ~rules:[ rule ] "distr_guards"
